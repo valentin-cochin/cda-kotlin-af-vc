d0fef4f996743bebfb862fce30f521228e623ee6
valentin-cochin <cochin.valentin@hotmail.fr>
2021-04-15 22:30:01 +0200

    Add function that produces a string of the current time in French

--- app/src/main/java/com/example/app/ClockReader.kt
+++ app/src/main/java/com/example/app/ClockReader.kt
@@ -1,5 +1,13 @@
 package com.example.app
 
-fun clockReader() {
-    println("time")
+import android.speech.tts.TextToSpeech
+import java.util.*
+
+fun clockReader(): String {
+    val timeNow: Calendar = Calendar.getInstance()
+    timeNow.timeZone = TimeZone.getTimeZone("Europe/Paris")
+    val hours = timeNow[Calendar.HOUR_OF_DAY]
+    val minutes = timeNow[Calendar.MINUTE]
+
+    return "Il est " + hours + " heures " + minutes + "minutes."
 }
